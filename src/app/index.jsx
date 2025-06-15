39a8fb3035f1e3952f97ebadb1af0695a58cb051
Xurshid4744 <nematoff4744@gmail.com>
2025-06-15 13:21:19 +0500

    fixed

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,10 +3,28 @@ import { useTelegramApp } from "@/hooks";
 import router from "@/routes";
 
 function App() {
-  const isTelegram = useTelegramApp();
+  const { user } = useTelegramApp();
   // if (!isTelegram) return <h1>Not Found</h1>;
 
-  return isTelegram;
+  return (
+    <>
+      APP
+      {user ? (
+        <div>
+          <h1>id:{user?.id}</h1>
+          <h1>is_bot:{user?.is_bot}</h1>
+          <h1>first_name:{user?.first_name}</h1>
+          <h1>last_name:{user?.last_name}</h1>
+          <h1>username:{user?.username}</h1>
+          <h1>language_code:{user?.language_code}</h1>
+          <h1>allows_write_to_pm:{user?.allows_write_to_pm}</h1>
+          <h1>photo_url:{user?.photo_url}</h1>
+        </div>
+      ) : (
+        <h1>USER NOT !</h1>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/hooks/useTelegramApp.jsx
+++ src/hooks/useTelegramApp.jsx
@@ -1,67 +1,23 @@
 import { useEffect, useState } from "react";
 import WebApp from "@twa-dev/sdk";
-import useUserStore from "@/store/user";
 
 const useTelegramApp = () => {
-  const updateUserState = useUserStore((state) => state.updateUserState);
-  const [isTelegram, setIsTelegram] = useState(true);
   const [user, setUser] = useState(null);
+
   useEffect(() => {
-    // if (!window.Telegram?.WebApp) {
-    //   setIsTelegram(true);
-    //   return;
-    // } else {
-    //   setIsTelegram(true);
-    // }
-    // WebApp.ready();
-    // const initDataUnsafe = WebApp.initDataUnsafe;
-    // const userr = initDataUnsafe.user;
-    // setUser(userr);
-    // // if (user) {
-    // //   updateUserState("id", user?.id);
-    // //   updateUserState("is_bot", user?.is_bot);
-    // //   updateUserState("first_name", user?.first_name);
-    // //   updateUserState("last_name", user?.last_name);
-    // //   updateUserState("username", user?.username);
-    // //   updateUserState("language_code", user?.language_code);
-    // //   updateUserState("allows_write_to_pm", user?.allows_write_to_pm);
-    // //   updateUserState("photo_url", user?.photo_url);
-    // // }
-    // if (!window.Telegram?.WebApp) {
-    //   setIsTelegram(false);
-    //   return;
-    // }
+    if (window.Telegram?.WebApp) {
+      WebApp.ready();
 
-    WebApp.ready();
-
-    const userData = WebApp.initDataUnsafe?.user;
-    if (!userData) {
-      setIsTelegram(false);
-      return;
+      const userData = WebApp.initDataUnsafe?.user;
+      if (userData) {
+        setUser(userData);
+      } else {
+        console.warn("Foydalanuvchi ma'lumotlari yo‘q");
+      }
     }
-
-    setUser(userData);
   }, []);
 
-  return (
-    <>
-      {user ? (
-        <div>
-          <h1>id:{user?.id}</h1>
-          <h1>is_bot:{user?.is_bot}</h1>
-          <h1>first_name:{user?.first_name}</h1>
-          <h1>last_name:{user?.last_name}</h1>
-          <h1>username:{user?.username}</h1>
-          <h1>language_code:{user?.language_code}</h1>
-          <h1>allows_write_to_pm:{user?.allows_write_to_pm}</h1>
-          <h1>photo_url:{user?.photo_url}</h1>
-        </div>
-      ) : (
-        <h1>USER YOQ</h1>
-      )}
-    </>
-  );
-  // return isTelegram;
+  return { user };
 };
 
 export default useTelegramApp;
