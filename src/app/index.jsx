518f95296b18bae7942bfc3df881ac8effc5e926
Xurshid4744 <nematoff4744@gmail.com>
2025-06-19 22:34:52 +0500

    fixed

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,31 +1,11 @@
-// src/App.tsx
+import { RouterProvider } from "react-router-dom";
 import { useTelegramApp } from "@/hooks";
-import React from "react";
+import router from "@/routes";
 
-const App = () => {
-  const { user } = useTelegramApp();
-
-  if (!user) {
-    return <p>❌ Iltimos, Telegram WebApp orqali kiring.</p>;
-  }
-
-  return (
-    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
-      <h1>👤 Telegram Foydalanuvchisi</h1>
-      <ul>
-        <li>ID: {user.id}</li>
-        <li>Ism: {user.first_name}</li>
-        {user.last_name && <li>Familiya: {user.last_name}</li>}
-        {user.username && <li>Username: @{user.username}</li>}
-        <li>Tili: {user.language_code}</li>
-        {user.photo_url && (
-          <li>
-            Avatar: <img src={user.photo_url} width="80" height="80" alt="avatar" />
-          </li>
-        )}
-      </ul>
-    </div>
-  );
-};
+function App() {
+  const isTelegram = useTelegramApp();
+  if (!isTelegram) return <h1>Not Found</h1>;
+  return <RouterProvider router={router} />;
+}
 
 export default App;
