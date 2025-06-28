d049af3ef05f24455ce8fd8af5ec3d6f2f0e68db
Xurshid4744 <nematoff4744@gmail.com>
2025-06-29 00:33:55 +0500

    fixed

--- src/components/menu-bar/index.jsx
+++ src/components/menu-bar/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { DiamondPlus, Folders, ShoppingBag } from "lucide-react";
+import { DiamondPlus, Folders, Plus, ShoppingBag } from "lucide-react";
 import "./index.scss";
 const MenuBar = () => {
   return (
     <div className="menu-bar">
       <NavLink to={"/cart"} className="menu-bar-link" s>
-        <ShoppingBag size={20}/>
+        <ShoppingBag size={35}/>
       </NavLink>
       <NavLink to={"/"} className="menu-bar-link" s>
-        <DiamondPlus size={20}/>
+        <Plus size={35}/>
       </NavLink>
       <NavLink to={"/orders"} className="menu-bar-link" s>
-        <Folders size={20}/>
+        <Folders size={35}/>
       </NavLink>
     </div>
   );
