ca39fdc226042308d780280101121fe2f5b13ae8
Xurshid4744 <nematoff4744@gmail.com>
2025-06-28 18:11:27 +0500

    fixed

--- src/components/product-card/footer-section.jsx
+++ src/components/product-card/footer-section.jsx
@@ -3,7 +3,9 @@ import AddCart from "../add-cart";
 
 const FooterSection = ({ total, count, inc, dic }) => (
   <div className="product-card-footer">
-    <p className="product-card-total">{formatAmount(total)} UZS</p>
+    <p className="product-card-total">
+      {total > 0 && formatAmount(total) + " UZS"}
+    </p>
     <div className="product-card-add-cart">
       <AddCart dic={dic} inc={inc} count={count} />
     </div>
