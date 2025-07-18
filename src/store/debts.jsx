365acb09f078541144a5851cf4f999a37ec28805
Xurshid Nematov <xurshid.nematov@zaytuntech.uz>
2025-07-18 11:07:21 +0500

    fixed

--- src/store/debts.jsx
+++ src/store/debts.jsx
@@ -5,7 +5,7 @@ const useDebtsStore = create((set) => ({
   debts: null,
   fetchDebts: async (id) => {
     try {
-      const res = await axiosInstance.get(`/debt/${id}`);
+      const res = await axiosInstance.get(`/client/debt/${id}`);
       set({ debts: res.data });
     } catch (error) {
       console.error("Qarzlarni olishda xatolik:", error);
