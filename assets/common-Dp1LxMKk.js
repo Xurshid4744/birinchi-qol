df93b78971cf5104ad9f1094074d8d83ab8dd05a
Xurshidbekk <Xurshidbekk@users.noreply.github.com>
2025-07-18 03:43:57 +0000

    deploy: bb9e7b98a02c588a260a9b6a82b94106a40203ea

---
+++ assets/common-Dp1LxMKk.js
@@ -0,0 +1,1 @@
+const s=3e5,E=5e4;export{E as D,s as M};
---
+++ assets/index-DP-Pxz1q.js
@@ -0,0 +1,6 @@
+import{c as l,b as i,j as e,L as t}from"./index-fHDuIQsK.js";/**
+ * @license lucide-react v0.514.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],o=l("arrow-left",d),x=({to:s,title:a,clear:r=!1})=>{const c=i(n=>n.clearOrders);return e.jsxs("div",{className:"menu-header",children:[e.jsxs("div",{className:"flexItems gap10",children:[e.jsx(t,{to:s,className:"menu-header-icon",children:e.jsx(o,{size:30})}),e.jsx("h4",{children:a})]}),r&&e.jsx("div",{className:"flexItems gap10 clear",onClick:c,children:e.jsx("img",{src:"/assets/image/garbage-can.png",alt:"bin"})})]})};export{x as M};
