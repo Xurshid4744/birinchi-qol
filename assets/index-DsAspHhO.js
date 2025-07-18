bbe00dc1408357d56297a5d15f1589bccab83ecb
Xurshid Nematov <xurshid.nematov@zaytuntech.uz>
2025-07-18 17:32:34 +0500

    Updates

--- assets/index-DsAspHhO.js
+++ assets/index-DsAspHhO.js
@@ -1,4 +1,4 @@
-import{c as l,b as i,j as e,L as t}from"./index-C6PHzZl6.js";/**
+import{c as l,b as i,j as e,L as t}from"./index-BGvl13-O.js";/**
  * @license lucide-react v0.514.0 - ISC
  *
  * This source code is licensed under the ISC license.
