713bb399ca116edfa50554e16a80e19e37416305
Xurshid Nematov <xurshid.nematov@zaytuntech.uz>
2025-07-18 10:34:16 +0500

    fixed

--- src/hooks/useTelegramApp.jsx
+++ src/hooks/useTelegramApp.jsx
@@ -3,11 +3,22 @@ import WebApp from "@twa-dev/sdk";
 
 const useTelegramApp = () => {
   const [user, setUser] = useState(null);
-
+  const updateUserState = useUserStore((state) => state.updateUserState);
   useEffect(() => {
     WebApp.ready();
     const tgUser = WebApp.initDataUnsafe?.user;
     setUser(tgUser || null);
+
+    if (tgUser) {
+      updateUserState("id", tgUser?.id);
+      updateUserState("is_bot", tgUser?.is_bot);
+      updateUserState("first_name", tgUser?.first_name);
+      updateUserState("last_name", tgUser?.last_name);
+      updateUserState("username", tgUser?.username);
+      updateUserState("language_code", tgUser?.language_code);
+      updateUserState("allows_write_to_pm", tgUser?.allows_write_to_pm);
+      updateUserState("photo_url", tgUser?.photo_url);
+    }
   }, []);
 
   return { user };
