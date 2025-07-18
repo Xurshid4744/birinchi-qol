aa22d5053e5b4418ce34ace9ecfa2a29c4e6b0be
Xurshid Nematov <xurshid.nematov@zaytuntech.uz>
2025-07-18 14:38:29 +0500

    Updates

--- assets/index-CawOEqDz.js
+++ assets/index-CawOEqDz.js
@@ -1,4 +1,4 @@
-import{c as o,b as r,j as s,P as m,L as u,f as t}from"./index-DYyYcWKG.js";import{M as n}from"./common-Dp1LxMKk.js";import{M as d}from"./index-ByOLddzH.js";/**
+import{c as o,b as r,j as s,P as m,L as u,f as t}from"./index-BYjC-Z13.js";import{M as n}from"./common-Dp1LxMKk.js";import{M as d}from"./index-DFDKdabP.js";/**
  * @license lucide-react v0.514.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/index-DFDKdabP.js
+++ assets/index-DFDKdabP.js
@@ -1,4 +1,4 @@
-import{c as l,b as i,j as e,L as t}from"./index-DYyYcWKG.js";/**
+import{c as l,b as i,j as e,L as t}from"./index-BYjC-Z13.js";/**
  * @license lucide-react v0.514.0 - ISC
  *
  * This source code is licensed under the ISC license.
